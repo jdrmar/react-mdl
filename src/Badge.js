a00216c966a46597869164b82f53ea4b732b2058
Tommy Leunen <tommy.leunen+github@gmail.com>
2016-03-13 22:06:42 -0400

    changes Badge with SFC + tests with enzyme/chai

--- src/Badge.js
+++ src/Badge.js
@@ -1,41 +1,41 @@
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 
-class Badge extends React.Component {
-    static propTypes = {
-        children: PropTypes.oneOfType([
-            PropTypes.element,
-            PropTypes.string
-        ]),
-        text: PropTypes.oneOfType([
-            React.PropTypes.string,
-            React.PropTypes.number
-        ]),
-        overlap: PropTypes.bool,
-        noBackground: PropTypes.bool
-    };
+const propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.element,
+        PropTypes.string
+    ]),
+    text: PropTypes.oneOfType([
+        React.PropTypes.string,
+        React.PropTypes.number
+    ]),
+    overlap: PropTypes.bool,
+    noBackground: PropTypes.bool
+};
 
-    render() {
-        const { children, text, overlap, noBackground } = this.props;
+const Badge = (props) => {
+    const { children, text, overlap, noBackground } = props;
 
-        // No badge if no children
-        if (!React.Children.count(children)) return null;
+    // No badge if no children
+    if (!React.Children.count(children)) return null;
 
-        const element = typeof children === 'string'
-            ? <span>{children}</span>
-            : React.Children.only(children);
+    const element = typeof children === 'string'
+        ? <span>{children}</span>
+        : React.Children.only(children);
 
-        // No text -> No need of badge
-        if (text === null || typeof text === 'undefined') return element;
+    // No text -> No need of badge
+    if (text === null || typeof text === 'undefined') return element;
 
-        return React.cloneElement(element, {
-            className: classNames('mdl-badge', {
-                'mdl-badge--overlap': !!overlap,
-                'mdl-badge--no-background': !!noBackground
-            }),
-            'data-badge': text
-        });
-    }
-}
+    return React.cloneElement(element, {
+        className: classNames('mdl-badge', {
+            'mdl-badge--overlap': !!overlap,
+            'mdl-badge--no-background': !!noBackground
+        }),
+        'data-badge': text
+    });
+};
+
+Badge.propTypes = propTypes;
 
 export default Badge;
